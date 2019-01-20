704006ac6f05d9c0d0617f94ae2df48a4a8fd7d2
Klonan <11986037+Klonan@users.noreply.github.com>
2019-01-20 16:01:24 +0100

    Burner generator!

--- control.lua
+++ control.lua
@@ -1,6 +1,5 @@
 --require "util"
 require "scripts.turbine"
-require "scripts.burner-gen"
 
 script.on_event(defines.events.on_tick, function(event)
   local t = event.tick
@@ -14,35 +13,16 @@ script.on_event(defines.events.on_tick, function(event)
     tick_wind()
   end
   check_interfaces()
-  update_pots()
 end)
 
 function BuiltEntity(event)
   local entity = event.created_entity
-	if entity.name == "wind-turbine-2" then	
+	if entity.name == "wind-turbine-2" then
     built_interface(entity)
     return
 	end
-	if entity.name == "burner-generator" then	
-		on_built_burner_gen(entity)
-    return
-	end
 end
 
-function MinedEntity(event)
-	if event.entity.name == "burner-generator" then
-	local b = event.entity
-	local X = b.position.x 
-	local Y = b.position.y
-		local power = {}		
-		lid =	b.surface.find_entity("burner-generator-power",{X, Y})
-		if lid ~= nil then
-		lid.destroy()	end
-	end
-end
 
 script.on_event(defines.events.on_built_entity, BuiltEntity)
 script.on_event(defines.events.on_robot_built_entity, BuiltEntity)
-script.on_event(defines.events.on_pre_player_mined_item , MinedEntity)
-script.on_event(defines.events.on_entity_died , MinedEntity)
-script.on_event(defines.events.on_robot_pre_mined  , MinedEntity)
--- prototypes/burner-generator.lua
+++ prototypes/burner-generator.lua
@@ -20,7 +20,7 @@ data:extend({
     flags = {},
     subgroup = "inserter",
     order = "d-j",
-    place_result = "burner-generator-power",
+    place_result = "burner-generator",
     stack_size = 50
   },
 
@@ -32,22 +32,21 @@ data:extend({
     {
       {"boiler", 1},
       {"iron-plate", 4},
-        {"iron-gear-wheel", 5},
-        {"pipe", 4}
+      {"iron-gear-wheel", 5},
+      {"pipe", 4}
     },
     result = "burner-generator"
   },
   {
     type = "generator",
-    name = "burner-generator-power",
+    name = "burner-generator",
     icon = "__KS_Power__/graphics/icons/burner-generator-icon.png",
     icon_size = 32,
-    flags = {"placeable-neutral","player-creation", "placeable-off-grid", "not-on-map"},
+    flags = {"placeable-neutral","player-creation"},
     minable = {mining_time = 1, result = "burner-generator"},
     max_health = 300,
     corpse = "small-remnants",
-    effectivity = 1,
-    fluid_usage_per_tick = 0.25,
+    effectivity = 0.25,
     resistances =
     {
       {
@@ -55,51 +54,86 @@ data:extend({
         percent = 70
       }
     },
-    collision_box = {{-0.7, -0.6}, {0.7, 0.6}},
+    collision_box = {{-1.2, -0.8}, {1.2, 0.8}},
     selection_box = {{-1.5, -1}, {1.5, 1}},
-    selectable_in_game = false,
-    maximum_temperature = 165,
-    fluid_box =
-    {
-      base_area = 3,
-      pipe_connections =
-      {
-       -- { position = {0, 0.8} }
-      },
-      filter = "steam"
-    },
     energy_source =
     {
       type = "electric",
       usage_priority = "secondary-output"
     },
+    burner =
+    {
+      type = "burner",
+      fuel_inventory_size = 2,
+      effectivity = 0.25,
+      light_flicker = {intensity = 1, size = 0.5, color = {r=1.0, g=0.5, b=0}},
+      smoke =
+      {
+        {
+          name = "smoke",
+          frequency = 10,
+          --position = {0.05, 0.9},
+          north_position = {0.05, 0.9},
+          east_position = {0.05, 0.9},
+        },
+        {
+          name = "burner-generator-smoke",
+          frequency = 30,
+          --position = {0.05, 0.9},
+          north_position = {0.05, 0.9},
+          east_position = {0.05, 0.8},
+        },
+      }
+    },
     horizontal_animation =
     {
-      filename = "__KS_Power__/graphics/entity/blank.png",
-      width = 50,
-      height = 50,
-      frame_count = 1,
-      line_length = 8,
-      shift = {1.34, -0.06}
+      layers =
+      {
+
+        {
+        filename = "__KS_Power__/graphics/entity/burner-generator-v.png",
+        priority = "extra-high",
+        width = 93,
+        height = 112,
+        shift = util.by_pixel(12, -0.5),
+        frame_count = 1
+        },
+        --{
+        --  filename = "__KS_Power__/graphics/entity/boiler-fire-down.png",
+        --  priority = "extra-high",
+        --  line_length = 8,
+        --  width = 21,
+        --  height = 34,
+        --  frame_count = 48,
+        --  axially_symmetrical = false,
+        --  direction_count = 1,
+        --  shift = util.by_pixel(3,11)
+        --}
+      }
     },
     vertical_animation =
     {
-      filename = "__KS_Power__/graphics/entity/blank.png",
-      width = 60,
-      height = 60,
-      frame_count = 1,
-      line_length = 8,
-      shift = {0.812, 0.031}
-    },
-    smoke =
-    {
+      layers =
       {
-        name = "smoke",
-        north_position = {0, -1.3},
-        east_position = {0, -1.3},
-        deviation = {0, 0},
-        frequency = 0.001,
-        starting_vertical_speed = 0
+        {
+          filename = "__KS_Power__/graphics/entity/burner-generator-h.png",
+          priority = "extra-high",
+          width = 121,
+          height = 80,
+          shift = util.by_pixel(20, 4),
+          frame_count = 1
+        },
+        --{
+        --  filename = "__KS_Power__/graphics/entity/boiler-fire-down.png",
+        --  priority = "extra-high",
+        --  line_length = 8,
+        --  width = 21,
+        --  height = 34,
+        --  frame_count = 48,
+        --  axially_symmetrical = false,
+        --  direction_count = 1,
+        --  shift = util.by_pixel(5,17)
+        --}
       }
     },
     working_sound =
@@ -112,89 +146,40 @@ data:extend({
       match_speed_to_activity = true,
     },
     min_perceived_performance = 0.25,
-    performance_to_sound_speedup = 0.5
+    performance_to_sound_speedup = 0.5,
+    max_power_output = "250kW",
   },
 
 }
 )
 
-local boiler = util.table.deepcopy(data.raw["boiler"]["boiler"])
-boiler.name = "burner-generator"
-boiler.fluid_box.pipe_connections = {}
-boiler.fluid_box.base_area = 1.5
-boiler.energy_consumption = "451kW"
-boiler.minable = {mining_time = 1, result = "burner-generator"}
-boiler.structure.north =
+data:extend(
 {
-  layers =
   {
+    type = "trivial-smoke",
+    name = "burner-generator-smoke",
+    flags = {"not-on-map"},
+    duration = 100,
+    fade_in_duration = 0,
+    fade_away_duration = 20,
+    spread_duration = 200,
+    slow_down_factor = 0.5,
+    start_scale = 1,
+    end_scale = 0,
+    color = {r = 1, g = 1, b = 1, a = 1},
+    cyclic = false,
+    affected_by_wind = true,
+    animation =
     {
-      filename = "__KS_Power__/graphics/entity/burner-generator-h.png",
+      filename = "__base__/graphics/entity/flamethrower-fire-stream/flamethrower-explosion.png",
       priority = "extra-high",
-      width = 121,
-      height = 80,
-      shift = util.by_pixel(20, 4)
-    }
-  }
-}
-boiler.structure.south = boiler.structure.north
-boiler.structure.east =
-{
-  layers =
-  {
-    {
-    filename = "__KS_Power__/graphics/entity/burner-generator-v.png",
-    priority = "extra-high",
-    width = 93,
-    height = 112,
-    shift = util.by_pixel(12, -0.5),
-    }
-  }
-}
-boiler.structure.west = boiler.structure.east
-boiler.patch = nil
-boiler.fire.north =
-{
-  filename = "__KS_Power__/graphics/entity/boiler-fire-down.png",
-  priority = "extra-high",
-  line_length = 8,
-  width = 21,
-  height = 34,
-  frame_count = 48,
-  axially_symmetrical = false,
-  direction_count = 1,
-  shift = util.by_pixel(5,17)
-}
-boiler.fire.east =
-{
-  filename = "__KS_Power__/graphics/entity/boiler-fire-down.png",
-  priority = "extra-high",
-  line_length = 8,
-  width = 21,
-  height = 34,
-  frame_count = 48,
-  axially_symmetrical = false,
-  direction_count = 1,
-  shift = util.by_pixel(3,11)
-}
-boiler.fire.west = boiler.fire.east
-boiler.fire.south = boiler.fire.north
-boiler.fire_glow_flicker_enabled = false
-boiler.fire_glow.north =
-{
-  filename = "__KS_Power__/graphics/entity/boiler-fire-down.png",
-  priority = "extra-high",
-  line_length = 1,
-  width = 1,
-  height = 1,
-  frame_count = 1,
-  axially_symmetrical = false,
-  direction_count = 1
-}
-boiler.fire_glow.south = boiler.fire_glow.north
-boiler.fire_glow.east = boiler.fire_glow.north
-boiler.fire_glow.west = boiler.fire_glow.north
-boiler.energy_source.smoke[1].south_position = boiler.energy_source.smoke[1].north_position
-boiler.energy_source.smoke[1].west_position = boiler.energy_source.smoke[1].east_position
-boiler.energy_source.effectivity = 0.25
-data:extend({boiler})
+      width = 64,
+      height = 64,
+      frame_count = 32,
+      line_length = 8,
+      scale = 0.25,
+      animation_speed = 32 / 100,
+      blend_mode = "additive"
+    },
+  },
+})
