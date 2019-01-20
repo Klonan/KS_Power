18739898165d71aba87b2af25d10b074d6e44cd0
Klonan <11986037+Klonan@users.noreply.github.com>
2019-01-20 19:13:18 +0100

    et.cidk

--- control.lua
+++ control.lua
@@ -3,10 +3,10 @@ require "scripts.turbine"
 
 script.on_event(defines.events.on_tick, function(event)
   local t = event.tick
-  if (t % 25000) == 0 then
+  if (t % 24999) == 0 then
     change_wind_day()
   end
-  if (t % 2500) == 0 then
+  if (t % 2499) == 0 then
     change_wind_hour()
   end
   if (t % 249) == 0 then
@@ -18,8 +18,7 @@ end)
 function BuiltEntity(event)
   local entity = event.created_entity
 	if entity.name == "wind-turbine-2" then
-    built_interface(entity)
-    return
+    return built_interface(entity)
 	end
 end
 
