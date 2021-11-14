bc85ffd2b0a3a7a78e190398138bf980b1a377ee
snouz <snooz-x@hotmail.com>
2021-11-14 23:37:56 +0100

    Wind turbine hr anim and separate shadow

--- prototypes/turbine.lua
+++ prototypes/turbine.lua
@@ -50,16 +50,50 @@ data:extend({
     continuous_animation = true,
     energy_production = "15kW",
     energy_usage = "0kW",
-    animation =
-    {
-			filename = "__KS_Power__/graphics/entity/wind-turbine-2/wind-turbine-2-anim.png",
-			width = 175,
-			height = 175,
-			frame_count = 20,
-			line_length = 5,
-			shift = {1.7,-1.4},
-      animation_speed = 1
-		},
+    animation = {
+      layers = {
+        {
+          filename = "__KS_Power__/graphics/entity/wind-turbine-2/wind-turbine-2-anim.png",
+          width = 175,
+          height = 175,
+          frame_count = 20,
+          line_length = 5,
+          shift = {1.7,-1.4},
+          animation_speed = 1,
+          hr_version = {
+            filename = "__KS_Power__/graphics/entity/wind-turbine-2/hr-wind-turbine-2-anim.png",
+            width = 350,
+            height = 350,
+            scale = 0.5,
+            frame_count = 20,
+            line_length = 5,
+            shift = {1.7,-1.4},
+            animation_speed = 1,
+          },
+        },
+        {
+          filename = "__KS_Power__/graphics/entity/wind-turbine-2/wind-turbine-2-anim-shadow.png",
+          width = 175,
+          height = 175,
+          frame_count = 20,
+          line_length = 5,
+          shift = {1.7,-1.4},
+          animation_speed = 1,
+          draw_as_shadow = true,
+          hr_version = {
+            filename = "__KS_Power__/graphics/entity/wind-turbine-2/hr-wind-turbine-2-anim-shadow.png",
+            width = 350,
+            height = 350,
+            scale = 0.5,
+            frame_count = 20,
+            line_length = 5,
+            shift = {1.7,-1.4},
+            animation_speed = 1,
+            draw_as_shadow = true,
+          },
+        },
+      }
+    },
     vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
     working_sound =
     {
